8638edd0cd696af4a25a198b3abffa65d15e854c
jacobbelt <jacobbelt@mmc.school.nz>
2026-05-12 10:31:30 +1200

    Function to create single line decorated headings

---
+++ C_01_Make_Statement.py
@@ -0,0 +1,9 @@
+# Functions go here
+def make_statment(statement, deocration):
+    """Empasises headings by adding decartion
+    at the start and end"""
+
+    print(f"{deocration * 3} {statement} {deocration * 3}")
+
+# Main Routine goes here
+make_statment("Programming is Fun!", "👍")
